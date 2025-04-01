8010d5a7435fe5a6b18e642343a45bfd99191f0a
stoic-park <demige79@gmail.com>
2025-04-01 20:48:35 +0900

    feat: nomalizeVNode 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,11 +1,19 @@
+// 1. type, props, ...children을 매개변수로 받는 함수를 작성하세요.
+// 2. 반환값은 { type, props, children } 형태의 객체여야 합니다.
 export function createVNode(type, props, ...children) {
   return {
     type,
     props,
     children: children
+      // 평탄화
       .flat(Infinity)
+      // 조건부 필터링
       .filter(
-        (child) => child !== null && child !== undefined && child !== false,
+        (child) =>
+          child !== null &&
+          child !== undefined &&
+          child !== false &&
+          child !== true,
       ),
   };
 }
--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,26 @@
+// 주어진 가상 노드(vNode)를 표준화된 형태로 변환하는 역할을 합니다. 이 함수는 다양한 타입의 입력을 처리하여 일관된 형식의 가상 노드를 반환하여 DOM 조작이나 렌더링 과정에서 일관된 데이터 구조를 사용할 수 있도록 합니다.
+// 1. vNode가 null, undefined 또는 boolean 타입일 경우 빈 문자열을 반환합니다.
+// 2. vNode가 문자열 또는 숫자일 경우 문자열로 변환하여 반환합니다.
+// 3. vNode의 타입이 함수일 경우 해당 함수를 호출하여 반환된 결과를 재귀적으로 표준화합니다.
+// 4. 그 외의 경우, vNode의 자식 요소들을 재귀적으로 표준화하고, null 또는 undefined 값을 필터링하여 반환합니다.
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return "";
+  }
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+  // 함수형일 경우
+  if (typeof vNode.type === "function") {
+    const component = vNode.type({
+      ...vNode.props,
+      children: vNode.children,
+    });
+    return normalizeVNode(component);
+  }
+
+  return {
+    ...vNode,
+    children: vNode.children.map(normalizeVNode),
+  };
 }

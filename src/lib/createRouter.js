7f4d122db66f121e01af27e73c644f225e5ba26c
stoic-park <demige79@gmail.com>
2025-04-04 01:44:57 +0900

    feat: gh-pages 배포 관련 로직 추가

--- src/lib/createRouter.js
+++ src/lib/createRouter.js
@@ -1,14 +1,24 @@
 import { createObserver } from "./createObserver";
 
+const BASE_PATH =
+  process.env.NODE_ENV === "production" ? "/front_5th_chapter1-2" : "";
+
 export const createRouter = (routes) => {
   const { subscribe, notify } = createObserver();
 
-  const getPath = () => window.location.pathname;
+  const getPath = () => {
+    const fullPath = window.location.pathname;
+
+    return BASE_PATH && fullPath.startsWith(BASE_PATH)
+      ? fullPath.slice(BASE_PATH.length)
+      : fullPath;
+  };
 
   const getTarget = () => routes[getPath()];
 
   const push = (path) => {
-    window.history.pushState(null, null, path);
+    window.history.pushState(null, null, BASE_PATH + path);
+
     notify();
   };
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 import { defineConfig } from "vite";
-
+import path from "path";
 export default mergeConfig(
   defineConfig({
+    base:
+      process.env.NODE_ENV === "production" ? "/front_5th_chapter1-2/" : "/",
     esbuild: {
       jsxFactory: "createVNode",
     },
@@ -10,6 +12,14 @@ export default mergeConfig(
       esbuildOptions: {
         jsx: "transform",
         jsxFactory: "createVNode",
+      },
+    },
+    build: {
+      outDir: "dist",
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, "index.html"),
+        },
       },
     },
   }),
